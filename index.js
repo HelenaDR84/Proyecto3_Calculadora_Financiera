4458d1f329aa9ee67b7b99bb53502aab431df9b5
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2023-11-27 11:17:20 +0100

    Index.js

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ botones.forEach(function(boton) {
   boton.addEventListener('click', function() {
     let valor = boton.textContent.trim();
 
-    // Si el botón es un número, lo añade a la pantalla
     if (!isNaN(valor)) {
       pantalla.textContent += valor;
     }
-    // Si el botón es un operador, guardamos el operando y el operador
+
     else if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
       if (operador !== null) {
         operando2 = parseFloat(pantalla.textContent);
@@ -25,22 +24,21 @@ botones.forEach(function(boton) {
       operador = valor;
       pantalla.textContent = '';
     }
-    // Si el botón es '=', realiza la operación
     else if (valor === '=') {
       operando2 = parseFloat(pantalla.textContent);
       let resultado = eval(operando1 + operador + operando2);
       pantalla.textContent = resultado;
       operador = null;
     }
-    // Si el botón es 'C', limpia la pantalla
+
     else if (valor === 'C') {
       pantalla.textContent = '';
     }
-    // Si el botón es '←', borra el último número
+
     else if (valor === '←') {
       pantalla.textContent = pantalla.textContent.slice(0, -1);
     }
-    // Si el botón es '.', añade un punto decimal
+      
     else if (valor === '.') {
       if (!pantalla.textContent.includes('.')) {
         pantalla.textContent += '.';
